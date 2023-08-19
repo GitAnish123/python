3c004e5067e5844a0cb45e21142bef7bab2290c2
GitAnish123 <mr.anish2376@gmail.com>
2023-08-19 09:33:21 -0400

    Update magic_number.py

--- python_work/magic_number.py
+++ python_work/magic_number.py
@@ -43,3 +43,38 @@ ages_3 = 15
 ages_1 = 18
 print(ages_1 >= 21 or ages_3 >= 21)  # This example, none of the inequalities are correct, so it will be "False"
 # That is how you can compare multiple conditions.
+
+# --------PRACTICE--------------
+
+friends1_age = 14
+if friends1_age < 18:
+    print("I'm still not an adult.")  # True
+friends2_age = 21
+if friends1_age > friends2_age:
+    print("Haha!!!, i'm older than you!") #False
+
+future_driver_age = 18
+if future_driver_age >= 18:
+    print("You are old enough to drive!") # True
+number = 2
+print(number == 5) # False
+main_number = 21
+if main_number != 21:
+    print("This is the wrong answer, please try again!")  # True
+
+number_of_shirts_in_Europe = 2_000_000_000_000_000_000_000_000
+if number_of_shirts_in_Europe <= 5_000_000_000_000_000_000_000_000_000_000_000_000_000:
+    print("Europe has less clothing than the USA.") # True
+print(100 < 201 and 202 >= 208)  # False
+print(9203 <= 21 or 382214823 == 382214823) # True
+top5_coding_languages = ['Javascript','C','C++','Python','Lua']
+print('Javascript' in top5_coding_languages)  # True
+print('SQL' in top5_coding_languages)  # False
+
+top5_coding_languages = ['Javascript','C','C++','Python','Lua']
+if 'HTML' not in top5_coding_languages:
+    print("This is not my top five coding languages.")
+top5_coding_languages = ['Javascript','C','C++','Python','Lua']
+if 'Javascript' in top5_coding_languages:
+    print("This is one of my top five favorite languages.")
+# This is examples of conditional testing.
