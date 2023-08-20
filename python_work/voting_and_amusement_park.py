95250cf387262306c7da9e02da92ee095bedec9c
GitAnish123 <mr.anish2376@gmail.com>
2023-08-20 09:37:29 -0400

    Create voting_and_amusement_park.py

---
+++ python_work/voting_and_amusement_park.py
@@ -0,0 +1,19 @@
+age = 19
+if age >= 18:
+    print("You are old enough to vote!")
+
+age = 19
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are to young to vote.")
+    print("Please register as soon you turn 18 years old!")
+# This is an 'if-else' statement. This is very useful.
+# This is good for TWO results 
+
