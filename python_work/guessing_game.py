b8101e7da5eab547c4d3fe006bd305ab6c33306c
GitAnish123 <mr.anish2376@gmail.com>
2023-09-18 20:21:35 -0400

    Create guessing_game.py

---
+++ python_work/guessing_game.py
@@ -0,0 +1,21 @@
+import random
+
+# This is a fun guessing game!
+# Please play this game!
+
+while True:
+    name = input("What is your name?: ")
+    your_choice = input("Lets play the guessing game! If you guess the number between 1-10, you get a prize!\nPlease guess a number between 1-10: ")
+    possible_actions = ['1','2','3','4','5','6','7','8','9','10']
+    computer_action = random.choice(possible_actions)
+    print(f"\nYou chose {your_choice}, computer chose {computer_action}.\n")
+    game_host_name = "Anish Pasumarthi"
+    
+    if your_choice == computer_action:
+        print(f"Congrats {name.title()}, You WON the game, run to {game_host_name.title()} to get your prize")
+    else:
+        print(f"Sorry {name.title()}, you DID NOT win the game.")
+
+    play_again = input("Thanks for playing!\nDo you want to play again? Type 'yes' to play again. Otherwise, type ANYTHING to quit the game.")
+    if play_again != 'yes':
+        break
