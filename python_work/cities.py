2ad885f189fc519c338ed41b510ac71d93b36fa8
GitAnish123 <mr.anish2376@gmail.com>
2023-09-28 17:58:53 -0400

    Create cities.py

---
+++ python_work/cities.py
@@ -0,0 +1,14 @@
+# Lets do "while" loops but ending with a "break" statement.
+# For example,
+
+prompt = "\nPlease enter the name of a city you have visited:"
+prompt += "\n(Enter 'quit' when you are finished.) "
+
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print(f"I'd love to go to {city.title()}!")
+# That is how you do it.
