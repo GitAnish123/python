8f70fd284deb72590f2b0bfdff4cc9533c52a898
GitAnish123 <mr.anish2376@gmail.com>
2023-10-09 11:57:32 -0400

    Create pets.py

---
+++ python_work/pets.py
@@ -0,0 +1,15 @@
+# You can use the "remove()" method to remove any specific instances in a list.
+# Lets do the value "cat" for an example,
+""" for example: """
+
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+
+while 'cat' in pets:
+    pets.remove('cat')
+
+print(pets)
+
+
+# So we have our old list and our new list.
+# You can apply this situation to harder concepts.
