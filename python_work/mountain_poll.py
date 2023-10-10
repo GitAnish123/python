a48bd55d7590d6f9f39197ea00cd74bd9d1391e2
GitAnish123 <mr.anish2376@gmail.com>
2023-10-09 21:05:02 -0400

    Update mountain_poll.py

---
+++ python_work/mountain_poll.py
@@ -0,0 +1,31 @@
+# We can fill a dictionary with user input.
+# Lets also do it with a while loop too!
+
+# After the code is over, the code prints the results of the poll i'll create!
+# Here is an example of a mountain poll:
+
+
+responses = {}
+# Set a flag to indicate that polling is active.
+polling_active = True
+
+while polling_active:
+    # Prompt for the person's name and response.
+    name = input("\nWhat is your name? ")
+    response = input("Which mountain would you like to climb someday? ")
+
+    # Store the response in the dictionary.
+    responses[name] = response
+
+    # Find out if anyone else is going to take the poll.
+    repeat = input("Would you like to let another person respond? (yes/ no) ")
+    if repeat == 'no':
+        polling_active = False
+
+# Polling is complete. Show the results.
+print("\n--- Poll Results ---")
+for name, response in responses.items():
+    print(f"{name} would like to climb {response}.")
+
+
+# That is one example of how you do it.
