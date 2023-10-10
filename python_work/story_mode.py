c68b0d33d5cbc0e6b4d76b650e36913284feb05a
GitAnish123 <mr.anish2376@gmail.com>
2023-10-09 21:05:00 -0400

    Create story_mode.py

---
+++ python_work/story_mode.py
@@ -0,0 +1,204 @@
+def dfhjsksfjsdfh3i3u2tr38oiasdfhu34qofoqew8fq2f3u8alfjewuf832quofl32io8rfr3489crynfiouwndsuioqj8nrdhiouasyr47rwo():
+    
+    
+    message = input("HOW THE FUCK U FOUND THIS PIECE??!!: ")
+    if message != "IDK":
+        print("That's it you nigger ass bitch, its over!!! (fucking asshole)")
+        print("Game Over")
+    else:
+        name = input("Ohhh, my bad for underestimating your DUMBNESS, guess you are??? (your name): ")
+        print(f"Hello, {name.title()}...")
+        print("You do not belong here bitch!!!: ")
+        magic_word = input("If you say my MAGIC word, I shall reward you with $100000, if you fail, YOU DIE BITCH!!!: ")
+        
+        
+        
+        if magic_word == "dick":
+            print(f"Ok {name.title()}, I guess your aren't a nigger-ass dick! Your a skinny bitch cock! Here is ur $100000 and get outa here before I kill u!!!!!!!")
+            yes = input("Say 'yes' if you understand!: ")
+            if yes != "yes":
+                print("YOU SHALL DIE FOR DISRESPECTING ME!!!!!!")
+                print("Game Over")
+            else:
+                print("Ok, you win, here is an extra $500000 and you shall be my royal servent!!!")
+                print("Before, Here is some tests to qualify. Many failed!")
+                q1 = input("What is 1+1?: ")
+                q1 = int(q1)
+                if q1 == 2:
+                    print("Ok, you are correct! That was easy!")
+                    q2 = input("What kind of button will NEVER unbutton???: ")
+                    if q2 == "belly button":
+                        print("You are correct, you are ready for the LAST challenge!")
+                        
+                        
+                        q3 = input("Will you fight me?: ")
+                        if q3 != "yes":
+                            print("YOU SHALL BE SQUASHED AND RETURN TO THE HUMAN WORLD ASSHOLE! ur lucky i spared u")
+                            die = input("You understand: ")
+                            if die == "yes":
+                                print("YOU RETURNED TO THE HUMAN WORLD empty handed!!!")
+                                print("You Lost 50%")
+                            else:
+                                print("YOU WILL DIE FOPR DISRESPECTING ME FUCKING NIGGER!!!")
+                                print("Game Over")
+                        else:
+                            duel = input("Ok choose your stratergy, (offence OR defence): ")
+                            if duel != "defence":
+                                print("You lost the fight, the monster defended that attack and thrown you!!!")
+                                print("Game Over")
+                            else:
+                                print("Ok, you survived the attack by 1% !!! The monster is frustrated")
+                                print("The monster attacked you and now you are almost nearly dying! You find a potion and drank it! You are feeling better.")
+                                duel_2 = input("Choose your next stratergy: ")
+                                if duel_2 != "defence":
+                                    print("You lost the fight, the monster defended that attack and thrown you!!!")
+                                    print("Game Over")
+                                else:
+                                    print("You survived less than 1%. You find a potion but it is on the other side!")
+                                    chance = input("Will you get the potion? ('yes' or 'no'): ")
+                                    if chance != "yes":
+                                        print("The monster came to you and did a BIG BLOW and you died")
+                                        print("Game Over")
+                                    else:
+                                        print("You ran and drank the potion succesfully! You also gained some extra strength")
+                                        duel_3 = input("Choose your next stratergy: ")
+                                        
+                                        if duel_3 != "offence":
+                                            print("You are DEAD, you tried to defend the monster's attack, but your were not prepared and died!")
+                                            print("Game Over")
+                                        
+                                        else:
+                                            print("You punched the monster so hard, the monster collapsed and died! You Won the Fight!")
+                                            print("Now You have lots of money!!! (over 10 million dollars)")
+                                            
+                                            
+                                            q4 = input("Do you want to return to the human world? ('yes' or 'no'): ")
+                                            if q4 == "yes":
+                                                print("You went back to human world, got a good wife (Ellie Hanson), good job, good finance including houses and cars, and you have a very pleasant life")
+                                                print("Besides, you and your wife (Ellie Hanson) kissed for the first time!!!")
+                                                breakup = input("You want to break up with Ellie Hanson? ('yes' or 'no'): ")
+                                                if breakup == "yes":
+                                                    print("Ellie Hanson left you the next day after you told her 'I want to break up with you'")
+                                                    print("After a couple of weeks, you felt guilty and tried to make up with her")
+                                                    print("Ellie did not want to marry you again!")
+                                                    print("You felt really sad and died out of sadness!")
+                                                    print("Game Over")
+                                                else:
+                                                    print("Your life was at peace. Your life is very easy since you are rich!")
+                                                    print(f"{name.title()} and Ellie Hanson had many pleasant memories together!")
+                                                    print("\n\n\nIn summary, you had a happy life!")
+                                                    print("You Won")
+                                                    play_again = input("You want to play 'PART 2'?")
+                                                    if play_again == "yes":
+                                                        print("The game is about to start!")
+                                                        print("\n\n\n\n\n\nMeanwhile, you and your wife are enjoying the view.")
+                                                        print("You are enjoying your time of your life!")
+                                                        print("Then, you went inside to get a refill of coffee.")
+                                                        print("Then, when you came back outside, Ellie Hanson is gone")
+                                                        wife = "ellie hanson"
+                                                        tcasewife = wife.title()
+                                                        first_reaction = input("What will you do? ('search nearby' or 'call the cops'): ")
+                                                        if first_reaction == "search nearby":
+                                                            print("You tried, but some random person scared your life out of existence!")
+                                                            print("He choked you until you died!")
+                                                            print("Game Over")
+                                                        elif first_reaction == "call the cops":
+                                                            print("You managed to get into your house and called the cops to help you find your wife")
+                                                            print(f"You are wondering if {tcasewife} is ok and safe. Is she actually coming back? Is this a prank?")
+                                                            second_option = input("choose an option, 'Hopes she comes back later' or 'call the cops'?: ")
+                                                            if second_option == "Hopes she comes back later":
+                                                                print("You ignored her absence, hoping she'll come back.")
+                                                                print(f"The next day, you heard news that {tcasewife} is dead!")
+                                                                print("You got a heart attack at the sudden news and you died.")
+                                                                print("Game Over")
+                                                            else:
+                                                                print("You called the cops on her absence.")
+                                                                print("You are in a deep panic and you counted the numbers 1-20.")
+                                                                current_number = 1
+                                                                while current_number <= 20:
+                                                                    print(current_number)
+                                                                    current_number += 1
+                                                                print("The cops asked you to come to the station tomorrow.")
+                                                                print("The next day, you are at the police station")
+                                                                print("The cops are asking you questions...")
+                                                                cop_q1 = input("What is your first name in title case form: ")
+                                                                if cop_q1 == name.title():
+                                                                    print("Ok, lets proceed")
+                                                                    age = input("So, what year are you born? Current year is 2023 (full form)(example is 2023): ")
+                                                                    age = int(age)
+                                                                    if age <= 2005:
+                                                                        print("Ok, you are ready to take action!")
+                                                                        print("Lets find your wife.")
+                                                                        cop_q2 = input("What is your wife full name in title case form: ")
+                                                                        if cop_q2 == wife.title():
+                                                                            print("Yes! I think I found her!")
+                                                                            print("I rescued from the kidnap that Ethan did. Ethan is the kidnapper. He is a very famous one. We caught him and we arrested him. Sadly, you can't talk to him because it's a rule.")
+                                                                            print("So yeah, but I found your wife. She is waiting for you at home. We are sending her home after 1 day.")
+                                                                            cop_q3 = input("So please can you leave. This is a very serious question ('yes' or 'no'): ")
+                                                                            if cop_q3 == 'yes':
+                                                                                print(f"Thank you {name.title()}")
+                                                                                print("After that, you went home and waited for one day")
+                                                                                print(f"The next day you met with your wife, {tcasewife}")
+                                                                                print(f"{name.title()} and {wife.title()} kissed and went out for celebration. They had so much moments and celebrations today!")
+                                                                                the2nd_last_question = input("Your wife asked you if you can quit your job. Reply your answer: ('yes' or 'no') ")
+                                                                                if the2nd_last_question != "yes":
+                                                                                    print(f"Your wife, {wife.title()} is a bit sad, but understood. This worked for everybody.")
+                                                                                    last_question = input("Your wife asked you how many hours per day will you work. reply your answer in numeric form: ")
+                                                                                    last_question = int(last_question)
+                                                                                    if last_question <= 8:
+                                                                                        print("Your wife is fine with that.")
+                                                                                        print("She kissed you out of approval and excitment for the adventures they will face in the future.")
+                                                                                        print(f"{name} and {tcasewife}, lived happily ever after!!!")
+                                                                                        print("\n\n\nYou Won")
+                                                                                    else:
+                                                                                        print("Your wife says she will leave you now for working too much")
+                                                                                        print(f"You miss {tcasewife} so much, you died!")
+                                                                                        print("Game Over")
+                                                                                else:
+                                                                                    print("Your wife is happy.")
+                                                                                    print("After a few days, the cops took away your house")
+                                                                                    print("You and Ellie Hanson are really sad and you both commit suicide")
+                                                                                    print("Game Over")
+                                                                            else:
+                                                                                print("Do NOT DESRESPECT ME. You are under arrest.")
+                                                                                print("You died after one week of prison")
+                                                                                print("Game Over")
+                                                                        else:
+                                                                            print("Sorry, I did not heard of her!")
+                                                                            print("Please wait about 4-5 years because we have some other cases to do.")
+                                                                            print(f"After the cop stated that, you felt really upset and died because you couldn't bear the loss of your wife, {tcasewife}")
+                                                                            print("Game Over")
+                                                                    else:
+                                                                        print("Sorry, you cannot take action. Please contact a valid 18 year old!")   
+                                                                        print("After the cop stated that, you got very upset and lived a miserable life.")
+                                                                        print("You Lost 50%")
+                                                                else:
+                                                                    print(f"You are wrong, liar!!!! I saw your name tag just before and your name is {name.title()}")
+                                                                    print("You are a fucking liar. I will not help a liar. Go home!!!")
+                                                                    print("After the cop said that, you fell down, got a heart attack and died.")
+                                                                    print("Game Over")
+                                                        else:
+                                                            print("Sorry, that is not a valid answer.")
+                                                    else:
+                                                        print(f"We got it {name.title()}, You won the game! Hope you enjoy your day!")
+                                            else:
+                                                print("You lived there for a couple of days, enjoying the food there!")
+                                                print("Suddenly, a huge storm came. The storm rushed down your palace and burned it!")
+                                                print("You died in the accident!")
+                                                print("Game Over")
+                    
+                    else:
+                        print("That is a huge disrespect cock dick! You are surely a fucking asshole. You shall be in thew dungeons for the rest of ur life, DICK!!!")
+                        print("You Lost 50%")
+                else:
+                    take = input("You do not DESERVE to be my servent BITCH. You are very dumb. I will take that money!: ")
+                    if take == "ok":
+                        print("Ok, I will spare your life, but LEAVE ASSHOLE!!!!")
+                        print("You Lost 50%")
+                    else:
+                        print("YOU DISRESPECTED ME????!!?!?!??! YOU WILL DIE BITCH-ASS!!!")
+                        print("Game Over")
+        else:
+            print("PREPARE TO DIE, fucking ass nigger dick bitch cock!!! YOU WILL LEAVE THIS WORLD!!!")
+            print("Game Over")
+dfhjsksfjsdfh3i3u2tr38oiasdfhu34qofoqew8fq2f3u8alfjewuf832quofl32io8rfr3489crynfiouwndsuioqj8nrdhiouasyr47rwo()
