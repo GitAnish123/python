4bafc173ff761fa380dd98292426e4dc51be4640
GitAnish123 <mr.anish2376@gmail.com>
2023-10-12 17:50:34 -0400

    Create number.py

---
+++ python_work/number.py
@@ -0,0 +1,9 @@
+x, y, z = 4, 5, 7
+print(x)
+print (z)
+print (y)
+randomage = 14_099_901
+print(randomage)
+randomagebutnoUNDERSCORES = 14099901
+print(randomagebutnoUNDERSCORES)
+print(18913**912)
