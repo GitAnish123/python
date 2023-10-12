24bcac39c9f5f69658f85e705f68bf07f802e61d
GitAnish123 <mr.anish2376@gmail.com>
2023-10-12 17:50:31 -0400

    Delete numbers.py

--- python_work/numbers.py
+++
@@ -1,9 +0,0 @@
-x, y, z = 4, 5, 7
-print(x)
-print (z)
-print (y)
-randomage = 14_099_901
-print(randomage)
-randomagebutnoUNDERSCORES = 14099901
-print(randomagebutnoUNDERSCORES)
-print(18913**912)
