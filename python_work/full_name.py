d3def099a559bccb58674bbac58abfaa86a60629
GitAnish123 <mr.anish2376@gmail.com>
2023-10-30 17:56:25 -0400

    Update full_name.py

--- python_work/full_name.py
+++ python_work/full_name.py
@@ -22,3 +22,35 @@ def get_formatted_name(first_name, last_name):
 musician = get_formatted_name('anish', 'pasumarthi')
 print(musician)
 # That is how you do a simple example of returning a function using "return".
+
+
+
+
+
+# We can make an argument optional!
+# For example, if we want to enter our first, middle, and last name, we can expand this code.
+def get_formatted_name(first_name, middle_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = f"{first_name} {middle_name} {last_name}"
+    return full_name.title()
+
+musician = get_formatted_name('john', 'lee', 'hooker')
+print(musician)
+
+# But middle names are not always needed!
+# If you want to display your first and last name OR your first, middle, and last name, do this code!
+# For example,
+
+def get_formatted_name(first_name, last_name, middle_name=''):
+    """Return a full name, neatly formatted."""
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+    else:
+        full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
+musician = get_formatted_name('john', 'hooker', 'lee')
+print(musician)
+# That is an example of how you make an argument optional!
