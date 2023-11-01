3967b56e613069179422b58ef632058171caa301
GitAnish123 <mr.anish2376@gmail.com>
2023-11-01 17:40:26 -0400

    Update person.py

--- python_work/person.py
+++ python_work/person.py
@@ -9,3 +9,19 @@ def build_person(first_name, last_name):
 musician = build_person('jimi', 'hendrix')
 print(musician)
 # That is a simple example!
+
+
+# You can also add more information like age, or occupation. You can add optional values so if you want you can include info
+# Lets say for example, 'age'.
+# For example,
+
+def build_person(first_name, last_name, age=None):
+    """Return a dictionary of information about a person."""
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
+
+musician = build_person('jimi', 'hendrix', age=27)
+print(musician)
+# That is how you add multiple values and optional values.
