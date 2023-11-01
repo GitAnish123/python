34256537145b8248b49a638b7d3d7221e4cefe24
GitAnish123 <mr.anish2376@gmail.com>
2023-11-01 16:43:37 -0400

    Update person.py

---
+++ python_work/person.py
@@ -0,0 +1,11 @@
+# You can return dictionaries inside of a function!
+# For example:
+
+def build_person(first_name, last_name):
+    """Return a dictionary of information about a person."""
+    person = {'first': first_name, 'last': last_name}
+    return person
+
+musician = build_person('jimi', 'hendrix')
+print(musician)
+# That is a simple example!
