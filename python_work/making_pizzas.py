537ec9e46d021064a91f78737a6e979802f18a07
GitAnish123 <mr.anish2376@gmail.com>
2023-11-25 11:15:02 -0500

    Update making_pizzas.py

--- python_work/making_pizzas.py
+++ python_work/making_pizzas.py
@@ -1,3 +1,8 @@
+# Ways to import modules and functions!!!
+# Different teqniques and ways are below...
+
+
+
 # Importing the "pizza" module and using the module for the functions to be used.
 import pizza
 pizza.make_pizza(16, 'pepperoni')
@@ -15,3 +20,21 @@ from pizza import make_pizza as mp
 
 mp(16, 'pepperoni')
 mp(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+
+# You can also directly give a module an alias. This is an effietient way to call functions much quicker.
+import pizza as p
+
+p.make_pizza(16, 'pepperoni')
+p.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+
+# You can import all the functions from a module using an asterisk. This is not recommonded for larger modules.
+from pizza import *
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+
+
+# These are some ways and teqniques for importing functions and modules.
