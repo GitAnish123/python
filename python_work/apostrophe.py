9cf9e4c8196c2fa0268704d7227a1433f15bd19d
GitAnish123 <mr.anish2376@gmail.com>
2023-11-25 14:58:05 -0500

    Update apostrophe.py

--- python_work/apostrophe.py
+++ python_work/apostrophe.py
@@ -1,4 +1,4 @@
 message = "One of Python's strengths is its diverse community."
 print(message)
 incorrect_message = 'do not use the single layer quotation marks when using apostrophes, like the ones im using now.'
-print(message)
+print(incorrect_message)
