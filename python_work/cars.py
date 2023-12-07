b1e7bb232da2b77c3430ec310decce4d28d69546
GitAnish123 <mr.anish2376@gmail.com>
2023-12-07 17:47:28 -0500

    Update cars.py

--- python_work/cars.py
+++ python_work/cars.py
@@ -50,3 +50,58 @@ car = 'Audi'
 print(car != 'Audi')
 print(car != 'bmw')
 # This sign indicates "not equal to operator"
+
+
+
+
+
+# Lets create the car class with attributes & methods! To begin this journey of different learning and aspects of classes...
+# Lets create the basic car class with just one method!
+class Car:
+    """A simple attempt to represent a car."""
+     
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+my_new_car = Car('audi', 'a4', 2024)
+print(my_new_car.get_descriptive_name())
+# That is a simple representation of a car. Get ready for more updates and more learning.
+
+
+
+# Lets create a default value for a class. 
+# When an instance is created, attributes can be defined without being passed in as parameters.
+# These attributes can be defined in the "__init__()"" method, where they are assigned a default value.
+# Let’s add an attribute called odometer_reading that always starts with a value of 0. 
+# We’ll also add a method read_odometer() that helps us read each car’s odometer. Here is the example:
+
+class Car:
+
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+my_new_car = Car('audi', 'a4', 2024)
+print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
+# That is an example of how to create a default value for a class.
