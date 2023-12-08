b79aad7382c981de7fe5058ba55be70a88360672
GitAnish123 <mr.anish2376@gmail.com>
2023-12-08 18:24:04 -0500

    Update cars.py

--- python_work/cars.py
+++ python_work/cars.py
@@ -105,3 +105,92 @@ my_new_car = Car('audi', 'a4', 2024)
 print(my_new_car.get_descriptive_name())
 my_new_car.read_odometer()
 # That is an example of how to create a default value for a class.
+
+
+
+# You also can modify a method. There are three ways...
+# direct instance change, set value through a method, or increment the value.
+# Here is an example of directly changing the value by instance.
+
+class Car:
+
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+my_new_car = Car('audi', 'a4', 2024)
+print(my_new_car.get_descriptive_name())
+
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
+# That is a simple example of how you do it.
+
+
+
+# Lets learn how to set a value through a method. Here is a simple example:
+class Car:
+
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+    def update_odometer(self, mileage):
+        """Set the odometer reading to the given value."""
+        self.odometer_reading = mileage
+
+my_new_car = Car('audi', 'a4', 2024)
+print(my_new_car.get_descriptive_name())
+
+my_new_car.update_odometer(23)
+my_new_car.read_odometer()
+
+# We can extend the method update_odometer() to do additional work every time the odometer reading is modified. 
+# Let’s add a little logic to make sure no one tries to roll back the odometer reading:
+
+class Car:
+
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+    def update_odometer(self, mileage):
+        """Set the odometer reading to the given value."""
+        if self.odometer_reading >= mileage:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+# That is an example of updating and adding logic by adding more methods!
