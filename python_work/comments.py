7b15f2836575500590fa0c177a0ccb308df73bbd
GitAnish123 <mr.anish2376@gmail.com>
2023-12-17 11:12:09 -0500

    Update comments.py

--- python_work/comments.py
+++ python_work/comments.py
@@ -1,4 +1,5 @@
 # Say hello to Anish Ram Pasumarthi, A rising Python coding master!!!
 print("Hello, Anish")
+
 # He is working hard and getting things done for the goal to happen!
 print("Anish is trying his best.")
