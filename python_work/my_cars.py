abfdad419e6b10a9774df125af3235b3d3164398
GitAnish123 <mr.anish2376@gmail.com>
2023-12-24 15:19:38 -0500

    Update my_cars.py

--- python_work/my_cars.py
+++ python_work/my_cars.py
@@ -6,3 +6,14 @@ my_mustang = Car('ford', 'mustang', 2024)
 print(my_mustang.get_descriptive_name())
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
+
+
+
+# You also can import an entire module using dot notation. Here is an example:
+import car
+
+my_mustang = car.Car('ford', 'mustang', 2024)
+print(my_mustang.get_descriptive_name())
+
+my_leaf = car.ElectricCar('nissan', 'leaf', 2024)
+print(my_leaf.get_descriptive_name())
