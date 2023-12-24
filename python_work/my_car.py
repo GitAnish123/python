3cda881beaa60dd96eaacaccb37179d10c824f16
GitAnish123 <mr.anish2376@gmail.com>
2023-12-24 13:03:27 -0500

    Create my_car.py

---
+++ python_work/my_car.py
@@ -0,0 +1,9 @@
+# Imporing one single class from a module, creating an instance, and using methods!
+
+from car import Car
+
+my_new_car = Car('audi', 'a4', 2024)
+print(my_new_car.get_descriptive_name())
+
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
