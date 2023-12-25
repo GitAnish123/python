06e601713dd6615b8f7c8ef51a87a2cd9278a0a5
GitAnish123 <mr.anish2376@gmail.com>
2023-12-25 11:44:56 -0500

    Update car.py

--- python_work/car.py
+++ python_work/car.py
@@ -102,3 +102,48 @@ class ElectricCar(Car):
         """
         super().__init__(make, model, year)
         self.battery = Battery()
+
+
+
+
+
+"""
+Sometimes you'll want to spread out your classes over several modules to keep any one file from growing too large 
+and avoid storing unrelated classes in the same module. When you store your classes in several modules, 
+you may find that a class in one module depends on a class in another module. 
+When this happens, you can import the required class into the first module.
+"""
+# Lets update this module so it only has one 'Car' class. This is useful when importing a module into a module!
+
+class Car:
+    """A simple attempt to represent a car."""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
