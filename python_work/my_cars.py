e570bcc59ff3139608c7ba141ca9c731ffd2777b
GitAnish123 <mr.anish2376@gmail.com>
2023-12-25 11:44:52 -0500

    Update my_cars.py

--- python_work/my_cars.py
+++ python_work/my_cars.py
@@ -28,3 +28,23 @@ from car import *
 
 print(my_mustang.get_descriptive_name())
 print(my_leaf.get_descriptive_name())
+
+
+
+
+
+
+# Now we can import from each module separately and create whatever kind of car we need.
+# We are now about to import a module into a module!          (Use 'new_electric_car.py' and 'car.py' for reference.)
+# Here is the example:
+
+from car import Car
+from new_electric_car import ElectricCar
+
+my_mustang = Car('ford', 'mustang', 2024)
+print(my_mustang.get_descriptive_name())
+
+my_leaf = ElectricCar('nissan', 'leaf', 2024)
+print(my_leaf.get_descriptive_name())
+# That is an example of how those kinds of advanced importing works!
+# This could apply for real-world situations too!
