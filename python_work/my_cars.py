38b3fbf9d9e9485f7b353b9d9d5fb745a89f477e
GitAnish123 <mr.anish2376@gmail.com>
2023-12-25 11:25:50 -0500

    Update my_cars.py

--- python_work/my_cars.py
+++ python_work/my_cars.py
@@ -10,10 +10,21 @@ print(my_leaf.get_descriptive_name())
 
 
 # You also can import an entire module using dot notation. Here is an example:
+
 import car
 
 my_mustang = car.Car('ford', 'mustang', 2024)
 print(my_mustang.get_descriptive_name())
 
 my_leaf = car.ElectricCar('nissan', 'leaf', 2024)
+print(my_leaf.get_descriptive_name())
+
+
+
+# Here is how you import ALL classes from a module. 
+# Import entire modules instead of this for clean imports and preventing name conflicts in your program file.
+
+from car import *
+
+print(my_mustang.get_descriptive_name())
 print(my_leaf.get_descriptive_name())
