ce75aec2d529e9edb0b484f184983d44bbaf6570
GitAnish123 <mr.anish2376@gmail.com>
2024-01-03 14:49:08 -0500

    Update chapter_reviews_not_in_order.py

--- python_work/chapter_reviews_not_in_order.py
+++ python_work/chapter_reviews_not_in_order.py
@@ -1545,3 +1545,185 @@ where one function ends and the next one begins. All import statements should be
 The only exception is if you use comments at the beginning of your file to describe the overall program.
 """
 # These are some tips & guidelines followed with examples to style your functions efficiently and as advanced as it will be.
+
+
+
+
+
+
+
+
+
+
+
+# Lets learn about classes
+"""
+Object-oriented programming (OOP) is one of the most effective approaches to writing software. 
+In object-oriented programming, you write classes that represent real-world things and situations, 
+and you create objects based on these classes. 
+When you write a class, you define the general behavior that a whole category of objects can have. 
+When you create individual objects from the class, each object is automatically equipped with the general behavior; 
+you can then give each object whatever unique traits you desire. 
+You'll be amazed how well real-world situations can be modeled with object-oriented programming.
+Making an object from a class is called instantiation, and you work with instances of a class. 
+In this chapter you'll write classes and create instances of those classes. 
+You'll specify the kind of information that can be stored in instances, 
+and you'll define actions that can be taken with these instances. 
+You'll also write classes that extend the functionality of existing classes, 
+so similar classes can share common functionality, and you can do more with less code. 
+You'll store your classes in modules and import classes written by other programmers into your own program files.
+Learning about object-oriented programming will help you see the world as a programmer does. 
+It'll help you understand your code—not just what's happening line by line, 
+but also the bigger concepts behind it. Knowing the logic behind classes will train you to think logically, 
+so you can write programs that effectively address almost any problem you encounter.
+Classes also make life easier for you and the other programmers you'll work with as you take on increasingly complex 
+challenges. When you and other programmers write code based on the same kind of logic, 
+you'll be able to understand each other's work. Your programs will make sense to the people you work with, 
+allowing everyone to accomplish more.
+"""
+
+# Lets create and use a class
+"""
+You can model almost anything using classes. 
+Let's start by writing a simple class, Dog, that represents a dog—not one dog in particular, but any dog. 
+What do we know about most pet dogs? Well, they all have a name and an age. 
+We also know that most dogs sit and roll over. 
+Those two pieces of information (name and age) and those two behaviors (sit and roll over) will go in our Dog class 
+because they're common to most dogs. This class will tell Python how to make an object representing a dog. 
+After our class is written, we'll use it to make individual instances, each of which represents one specific dog.
+"""
+# Each instance created from the Dog class will store a name and an age.
+# We’ll give each dog the ability to sit() and roll_over()
+# Here is a simple example:
+
+class Dog:
+    """A simple attempt to model a dog"""
+
+    def __init__(self, name, age):
+        """Initialize name and age attributes."""
+        self.name = name
+        self.age = age
+    
+    def sit(self):
+        """Stimulate sitting in response to a command."""
+        print(f"{self.name} is now sitting.")
+    
+    def roll_over(self):
+        """Stimulate a dog rolling over."""
+        print(f"{self.name} rolled over.")
+
+"""
+We first define a class called Dog ❶. 
+By convention, capitalized names refer to classes in Python. 
+There are no parentheses in the class definition because we're creating this class from scratch.
+We then write a docstring describing what this class does.
+A function that's part of a class is a method. 
+Everything you learned about functions applies to methods as well; 
+the only practical difference for now is the way we'll call methods. 
+The __init__() method ❷ is a special method that Python runs automatically whenever we create a new instance based 
+on the Dog class. 
+This method has two leading underscores and two trailing underscores, 
+a convention that helps prevent Python's default method names from conflicting with your method names. 
+Make sure to use two underscores on each side of __init__(). If you use just one on each side, 
+the method won't be called automatically when you use your class, 
+which can result in errors that are difficult to identify. We define the __init__() method to have three parameters: 
+self, name, and age. The self parameter is required in the method definition, 
+and it must come first, before the other parameters. 
+It must be included in the definition because when Python calls this method later (to create an instance of Dog), 
+the method call will automatically pass the self argument. 
+Every method call associated with an instance automatically passes self, 
+which is a reference to the instance itself; 
+it gives the individual instance access to the attributes and methods in the class. 
+When we make an instance of Dog, Python will call the __init__() method from the Dog class. 
+We'll pass Dog() a name and an age as arguments; self is passed automatically, 
+so we don't need to pass it. Whenever we want to make an instance from the Dog class, 
+we'll provide values for only the last two parameters, name and age. The two variables defined in the body of the 
+__init__() method each have the prefix self ❸. 
+Any variable prefixed with self is available to every method in the class, 
+and we'll also be able to access these variables through any instance created from the class. 
+The line self.name = name takes the value associated with the parameter name and assigns it to the variable name, 
+which is then attached to the instance being created. The same process happens with self.age = age. 
+Variables that are accessible through instances like this are called attributes. The Dog class has two other 
+methods defined: sit() and roll_over() ❹. Because these methods don't need additional information to run, 
+we just define them to have one parameter, self. The instances we create later will have access to these methods. 
+In other words, they'll be able to sit and roll over. For now, sit() and roll_over() don't do much. 
+They simply print a message saying the dog is sitting or rolling over. 
+But the concept can be extended to realistic situations: 
+if this class were part of a computer game, these methods would contain code to make an animated dog sit and roll over. 
+If this class was written to control a robot, 
+these methods would direct movements that cause a robotic dog to sit and roll over.
+"""
+
+
+
+# We can make an instance/object from a class.
+# Think of a class as a set of instructions for how to make an instance. 
+# The Dog class is a set of instructions that tells Python how to make individual instances representing specific dogs.
+# Let’s make an instance representing a specific dog:
+
+class Dog:
+    """A simple attempt to model a dog"""
+
+    def __init__(self, name, age):
+        """Initialize name and age attributes."""
+        self.name = name
+        self.age = age
+    
+    def sit(self):
+        """Stimulate sitting in response to a command."""
+        print(f"{self.name} is now sitting.")
+    
+    def roll_over(self):
+        """Stimulate a dog rolling over."""
+        print(f"{self.name} rolled over.")
+
+my_dog = Dog('Willie', 6)  # creating instance
+
+print(f"My dog's name is {my_dog.name}.")   # Using attribute
+print(f"My dog is {my_dog.age} years old.")  # Using attribute
+"""
+The Dog class we're using here is the one we just wrote in the previous example. 
+Here, we tell Python to create a dog whose name is 'Willie' and whose age is 6 ❶. 
+When Python reads this line, it calls the __init__() method in Dog with the arguments 'Willie' and 6. 
+The __init__() method creates an instance representing this particular dog and sets the name and age attributes using 
+the values we provided. Python then returns an instance representing this dog. 
+We assign that instance to the variable my_dog. 
+The naming convention is helpful here; we can usually assume that a capitalized name like Dog refers to a class, 
+and a lowercase name like my_dog refers to a single instance created from a class.
+"""
+
+
+
+# You can access attributes from an instance by using dot notation.
+print(my_dog.name)
+print(my_dog.age)
+# Dot notation is used often in Python and demonstrates how it works and is crucial.
+
+
+
+# You can use dot notation to call methods from an instance.
+class Dog:
+    """A simple attempt to model a dog"""
+
+    def __init__(self, name, age):
+        """Initialize name and age attributes."""
+        self.name = name
+        self.age = age
+    
+    def sit(self):
+        """Stimulate sitting in response to a command."""
+        print(f"{self.name} is now sitting.")
+    
+    def roll_over(self):
+        """Stimulate a dog rolling over."""
+        print(f"{self.name} rolled over.")
+
+my_dog = Dog('Willie', 6)
+my_dog.sit()
+my_dog.roll_over()
+"""
+To call a method, give the name of the instance (in this case, my_dog) and the method you want to call, 
+separated by a dot. When Python reads my_dog.sit(), 
+it looks for the method sit() in the class Dog and runs that code. 
+Python interprets the line my_dog.roll_over() in the same way. Now Willie does what we tell him to.
+"""
