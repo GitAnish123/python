7d7c1ab2d69521f61a9b02e1644655837e09caaa
GitAnish123 <mr.anish2376@gmail.com>
2025-10-11 11:06:00 -0400

    Update app.py

--- python_work/food_scanner_app/app.py
+++ python_work/food_scanner_app/app.py
@@ -75,7 +75,50 @@ if uploaded_file is not None:
 
     # Text labels (thousands of common foods)
     # This is just a sample; you can expand the list as needed
-    food_labels = ["pizza", "hamburger", "hot dog", "ice cream", "donut", "cake", "spaghetti", "sushi", "salad", "banana"]
+    food_labels = [
+    'pizza', 'hamburger', 'hot dog', 'ice cream', 'donut', 'cake', 'spaghetti', 'salad',
+    'banana bread', 'apple pie', 'sandwich', 'pasta', 'fried chicken', 'steak', 'pancakes',
+    'waffles', 'soup', 'chocolate', 'cheese', 'bagel', 'cookies', 'mac and cheese',
+    'lasagna', 'carbonara', 'bolognese', 'alfredo pasta', 'grilled cheese', 'fish and chips',
+    'mashed potatoes', 'roast beef', 'beef wellington', 'yorkshire pudding',
+    'brownies', 'cupcakes', 'muffins', 'bread pudding', 'milkshake', 'smoothie', 'coffee',
+    'tea', 'iced tea', 'lemonade', 'soda', 'cola', 'root beer', 'eggs', 'toast', 'cereal',
+    'bacon', 'sausage', 'omelette', 'scrambled eggs', 'fried eggs', 'buttered toast',
+    'peanut butter sandwich', 'grilled chicken', 'roast chicken', 'baked potato', 'cornbread',
+    'biscuits', 'pasta salad', 'fruit salad', 'coleslaw', 'potato salad', 'vegetable soup',
+    'chicken soup', 'tomato soup', 'pumpkin soup', 'bread roll', 'pudding', 'ice cream sundae',
+    'milk', 'butter', 'yogurt', 'jam', 'honey', 'oats', 'toast with jam',
+    'biryani', 'chicken biryani', 'mutton biryani', 'vegetable biryani', 'rogan josh',
+    'korma', 'butter chicken', 'chicken tikka masala', 'chicken tikka', 'paneer tikka',
+    'paneer butter masala', 'dal makhani', 'dal fry', 'dal tadka', 'chole bhature',
+    'chole masala', 'rajma masala', 'aloo gobi', 'bhindi masala', 'saag paneer', 'palak paneer',
+    'malai kofta', 'kofta', 'vindaloo', 'lamb curry', 'fish curry', 'egg curry', 'prawn curry',
+    'tandoori chicken', 'naan', 'butter naan', 'garlic naan', 'roti', 'chapati', 'paratha',
+    'lachha paratha', 'rumali roti', 'puri', 'bhature', 'kulcha', 'pav', 'pav bhaji', 'vada pav',
+    'misal pav', 'poha', 'upma', 'idli', 'dosa', 'masala dosa', 'uttapam', 'medu vada',
+    'sambar', 'rasam', 'thepla', 'dhokla', 'khandvi', 'fafda', 'kachori', 'samosa', 'pakora',
+    'bonda', 'pani puri', 'golgappa', 'sev puri', 'dahi puri', 'chaat', 'masala peanuts',
+    'pulao', 'jeera rice', 'fried rice veg', 'paneer manchurian', 'bread pakora',
+    'grilled sandwich', 'cheese sandwich', 'burger veg', 'burger chicken', 'chai',
+    'masala chai', 'ginger chai', 'lassi', 'mango lassi', 'sweet lassi', 'falooda', 'kulfi',
+    'gulab jamun', 'rasgulla', 'jalebi', 'barfi', 'kaju katli', 'peda', 'modak', 'ladoos',
+    'besan ladoo', 'motichoor ladoo', 'soan papdi', 'imarti', 'basundi', 'rabri', 'phirni',
+    'kheer', 'shrikhand', 'puran poli', 'dal bati churma', 'halwa', 'suji halwa', 'gajar halwa',
+    'payasam', 'mango juice', 'coconut water', 'pomegranate juice', 'curd', 'raita',
+    'boondi raita', 'cucumber raita', 'papad', 'pickle', 'achar', 'onion salad',
+    'apple', 'banana', 'orange', 'grape', 'pear', 'peach', 'plum', 'cherry', 'strawberry',
+    'blueberry', 'raspberry', 'blackberry', 'watermelon', 'cantaloupe', 'honeydew',
+    'pineapple', 'mango', 'papaya', 'guava', 'kiwi', 'lemon', 'lime', 'pomegranate',
+    'coconut', 'fig', 'date', 'apricot', 'tangerine', 'nectarine', 'cranberry', 'dragon fruit',
+    'passion fruit', 'jackfruit', 'lychee', 'star fruit', 'mulberry', 'custard apple', 'sapota',
+    'berries', 'avocado', 'grapefruit', 'potato', 'sweet potato', 'carrot', 'onion', 'tomato', 'garlic', 
+    'ginger', 'cucumber', 'spinach', 'lettuce', 'cabbage', 'cauliflower', 'broccoli', 'peas', 'green beans',
+    'bell pepper', 'capsicum', 'chili pepper', 'corn', 'eggplant', 'brinjal', 'okra', 'ladyfinger',
+    'pumpkin', 'zucchini', 'radish', 'beetroot', 'turnip', 'celery', 'leek', 'asparagus',
+    'artichoke', 'mushroom', 'kale', 'mustard greens', 'fenugreek leaves', 'drumstick',
+    'bottle gourd', 'ridge gourd', 'bitter gourd', 'coriander', 'mint', 'parsley', 'basil',
+    'spring onion', 'curry leaves'
+]
     text_inputs = torch.cat([clip.tokenize(f) for f in food_labels]).to(device)
 
     with torch.no_grad():
